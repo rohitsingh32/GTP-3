b09fcf816fac275bada539f46324a325fc8aa5d0
Rohit Singh <124232542+rohitsingh32@users.noreply.github.com>
2023-03-15 14:07:56 +0530

    GTP-3 commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Blog, Features, Footer, Header, Possibility, WhatGTP3} from './containers';
+import { Cta, Brand, Navbar } from './components'
+import './App.css'
+
+const App = () => {
+  return (
+  <div className='App'>
+      <div className='gradient__bg'>
+       <Navbar/>
+       <Header/>
+      </div>
+      <Brand/>
+      <WhatGTP3/>
+      <Features/>
+      <Possibility/>
+      <Cta/>
+      <Blog/>
+      <Footer/>
+  </div>
+
+  )
+}
+
+export default App
